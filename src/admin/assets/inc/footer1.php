956c5b069033bd936b9d3270ce50211c28fcf17a
xleozin-f5 <leandrodpereiras@gmail.com>
2024-05-05 17:49:19 +0100

    admin menu

---
+++ src/admin/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt">
+    2023 - <?php echo date('Y'); ?> &copy; Sistema de Marcações de Consultas Hospitalares. Desenvolvido pela QueryCare</a>
+</footer>
---
+++ src/admin/assets/inc/sidebar.php
@@ -0,0 +1,64 @@
+<div class="left-side-menu">
+    <div class="slimscroll-menu">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <ul class="metismenu" id="side-menu">
+
+                <li class="menu-title">QueryCare - menu</li>
+
+                <li>
+                    <a href="his_admin_dashboard.php">
+                        <i class="fe-airplay"></i>
+                        <span> Painel </span>
+                    </a>
+
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="fab fa-accessible-icon "></i>
+                        <span> Pacientes </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_register_patient.php">Registar Paciente</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_view_patients.php">Ver Pacientes</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_manage_patient.php">Gerir Pacientes</a>
+                        </li>
+                        <hr>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="mdi mdi-doctor"></i>
+                        <span> Médico </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_add_employee.php">Adicionar Médico</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_view_employee.php">Ver Médico</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_manage_employee.php">Gerir Médico</a>
+                        </li>
+                        <hr>
+                    </ul>
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
