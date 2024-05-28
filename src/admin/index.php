5b99ec7781429c69e02168387ca02bff89a16e2d
xleozin-f5 <leandrodpereiras@gmail.com>
2024-05-28 13:17:58 +0200

    orga

--- src/admin/index.php
+++ src/admin/index.php
@@ -47,9 +47,6 @@ if(isset($err)) {
     <meta charset="utf-8" />
     <title>QueryCare</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="" name="description" />
-    <meta content="" name="MartDevelopers" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
     <!-- App css -->
--- src/doc/index.php
+++ src/doc/index.php
@@ -35,9 +35,6 @@
     <meta charset="utf-8" />
     <title>QueryCare</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="" name="description" />
-    <meta content="" name="MartDevelopers" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
