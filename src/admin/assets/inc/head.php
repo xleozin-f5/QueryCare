d4b900293f81e6af4409487f85a602c87b4daf65
xleozin-f5 <leandrodpereiras@gmail.com>
2024-05-30 10:04:43 +0200

    idk

--- src/admin/assets/inc/head.php
+++ src/admin/assets/inc/head.php
@@ -2,8 +2,6 @@
     <meta charset="utf-8" />
     <title>QueryCare</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="MartDevelopers" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
